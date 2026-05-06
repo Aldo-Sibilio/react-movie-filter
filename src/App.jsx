ce4a81b0679772789e51a724f5d29325e1338bf6
Aldo Sibilio <sibilioaldo6@gmail.com>
2026-05-06 15:51:38 +0200

    creati array film e stati per gestione filtro

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState, useEffect } from "react";
+
+function App() {
+
+  // array film
+  const movies = [
+    { title: "Inception", genre: "Fantascienza" },
+    { title: "Il Padrino", genre: "Thriller" },
+    { title: "Titanic", genre: "Romantico" },
+    { title: "Batman", genre: "Azione" },
+    { title: "Interstellar", genre: "Fantascienza" },
+    { title: "Pulp Fiction", genre: "Thriller" }
+  ];
+
+  // stato select
+  const [selectedGenre, setSelectedGenre] = useState("");
+
+  // stato film filtrati
+  const [filteredMovies, setFilteredMovies] = useState(movies);
